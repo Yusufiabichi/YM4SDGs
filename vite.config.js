29b06f04368ca81829bb44a09787f0bfc7546d57
Yusufiabichi <yusufiabichi@yahoo.com>
2025-09-10 05:00:11 +0100

    virte config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
-
-
+  root: '.',  // ensure root points where index.html exists
+  build: {
+    outDir: 'dist'
+  }
+})
