e74fccc76724f68b7b0c8cda01f2ff97c5fca29f
Yusufiabichi <yusufiabichi@yahoo.com>
2026-01-10 16:54:17 +0100

    Build for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AppRoutes } from "./router";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
+import ScrollToTop from "./pages/home/components/ScrollToTop.tsx";
 
 
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <BrowserRouter basename={__BASE_PATH__}>
+      <ScrollToTop />
+      <Routes />
         <AppRoutes />
       </BrowserRouter>
     </I18nextProvider>
